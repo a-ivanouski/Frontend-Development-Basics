7fed8fb6e23563bb57c49251a186d59df7be2e8e
Dzmitry Patseyuk <dzmitry_patseyuk@epam.com>
2015-04-01 12:59:51 +0300

    fixed errors

--- 6. JavaScript Fundamentals/task1/script.js
+++ 6. JavaScript Fundamentals/task1/script.js
@@ -1,14 +1,25 @@
-function loadImage(id,url) {
-    var img = new Image();
-
-    img.onload = function () {
-        console.log('correct');
-        $('#' + id).attr('src', url);
+$.widget("image.load", {
+    _create: function () {
+        var element = this;
+        element._showSpiner(element);
+        this.element.on('load', function () {
+            element._show(element);
+        });
+        this.element.on('error', function () {
+            element._showError(element);
+        });
+    },
+    _showSpiner: function (element) {
+        element.element[0].style["background-image"] = "url('spiner.gif')";
+        element.element[0].style["background-size"] = "100% 100%";
+    },
+    _showError: function (element) {
+        element.element[0].src = 'error.jpg';
+    },
+    _show: function (element) {
+        element.element[0].style["background-size"] = "none";
     }
 
-    img.onerror = function () {
-        console.log('error');
-        $('#' + id).attr('src', 'error.jpg');
-    }
-    img.src = url;
-}
+});
+
+
