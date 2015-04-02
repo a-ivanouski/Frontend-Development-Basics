fb52c2d1fe796e7645d10c8839700019332ddfe6
Dzmitry Patseyuk <dzmitry_patseyuk@epam.com>
2015-04-02 11:58:08 +0300

    created new solution of task

--- 6. JavaScript Fundamentals/task1/script.js
+++ 6. JavaScript Fundamentals/task1/script.js
@@ -1,25 +1,7 @@
-$.widget("image.load", {
-    _create: function () {
-        var element = this;
-        element._showSpiner(element);
-        this.element.on('load', function () {
-            element._show(element);
-        });
-        this.element.on('error', function () {
-            element._showError(element);
-        });
-    },
-    _showSpiner: function (element) {
-        element.element[0].style["background-image"] = "url('spiner.gif')";
-        element.element[0].style["background-size"] = "100% 100%";
-    },
-    _showError: function (element) {
-        element.element[0].src = 'error.jpg';
-    },
-    _show: function (element) {
-        element.element[0].style["background-size"] = "none";
-    }
+ 
 
-});
+
+
+
 
 
