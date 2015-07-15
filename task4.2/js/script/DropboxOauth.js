dded0465ad34a078ea1aa67ec6a26c5b785a0a6e
Dzmitry Patseyuk <Dzmitry.Patseyuk@aerlingus.com>
2015-07-15 16:00:57 +0300

    space fix

--- task4.2/js/script/DropboxOauth.js
+++ task4.2/js/script/DropboxOauth.js
@@ -22,39 +22,39 @@ angular.module('dropboxModule', [])
                 .error(function (data, status, headers, config) {
                     alert("error");
                 });
-            }
+        }
 
-            function getProfileInfo(token) {
-                return sendRequest('GET', 'https://api.dropbox.com/1/account/info', '/', token);
-            }
+        function getProfileInfo(token) {
+            return sendRequest('GET', 'https://api.dropbox.com/1/account/info', '/', token);
+        }
 
-            function getFolderInfo(path, token) {
-                return sendRequest('GET', 'https://api.dropbox.com/1/metadata/auto', path, token);
-            }
+        function getFolderInfo(path, token) {
+            return sendRequest('GET', 'https://api.dropbox.com/1/metadata/auto', path, token);
+        }
 
-            function downloadFile(path, token) {
-                sendRequest('GET', 'https://api.dropbox.com/1/media/auto', path, token)
-                    .success(function (data, status, headers, config) {
-                        var a = document.createElement("a");
-                        a.setAttribute('href', data.url);
-                        a.click();
-                    })
-            }
+        function downloadFile(path, token) {
+            sendRequest('GET', 'https://api.dropbox.com/1/media/auto', path, token)
+                .success(function (data, status, headers, config) {
+                    var a = document.createElement("a");
+                    a.setAttribute('href', data.url);
+                    a.click();
+                })
+        }
 
-            function getPromiseToken(secret, appKey, appSecret) {
-                var url = 'https://api.dropbox.com/1/' + 'oauth2/token?code=' + secret + '&grant_type=authorization_code&client_id=' + appKey + '&client_secret=' + appSecret;
-                return sendRequest('POST', url);
-            }
+        function getPromiseToken(secret, appKey, appSecret) {
+            var url = 'https://api.dropbox.com/1/' + 'oauth2/token?code=' + secret + '&grant_type=authorization_code&client_id=' + appKey + '&client_secret=' + appSecret;
+            return sendRequest('POST', url);
+        }
 
-            function getLinkForToken(appKey) {
-                return "https://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=" + appKey;
-            }
+        function getLinkForToken(appKey) {
+            return "https://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=" + appKey;
+        }
 
-            return {
-                getLinkForToken: getLinkForToken,
-                getPromiseToken: getPromiseToken,
-                getProfileInfo: getProfileInfo,
-                getFolderInfo: getFolderInfo,
-                downloadFile: downloadFile,
-            }
+        return {
+            getLinkForToken: getLinkForToken,
+            getPromiseToken: getPromiseToken,
+            getProfileInfo: getProfileInfo,
+            getFolderInfo: getFolderInfo,
+            downloadFile: downloadFile,
+        }
     })
