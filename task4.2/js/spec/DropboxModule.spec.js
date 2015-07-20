b9b06f1aa5b14d8bfb0f4b2efda0004add9d65c9
Dzmitry Patseyuk <Dzmitry.Patseyuk@aerlingus.com>
2015-07-20 14:20:51 +0300

    added test for folder Info

--- task4.2/js/spec/DropboxModule.spec.js
+++ task4.2/js/spec/DropboxModule.spec.js
@@ -1,4 +1,4 @@
-describe('DropboxModule ', function() {
+describe('Tests for DropboxModule:', function() {
   var dropboxService = {};
   var $httpBackend;
   var $q;
@@ -17,61 +17,68 @@ describe('DropboxModule ', function() {
         email: 'email'
       };
 
-  var responseFolderInfo = {};
+  var responseFolderInfo = {
+      path: 'Folder',
+      contents: ['file1','file2','file3']
+  };
 
   var urlForToken = 'https://api.dropbox.com/1/' + 'oauth2/token?code=' + secret + '&grant_type=authorization_code&client_id=' + appKey + '&client_secret=' + appSecret;
   var urlForProfileInfo = 'https://api.dropbox.com/1/account/info/';
   var uarForFolderInfo = 'https://api.dropbox.com/1/metadata/auto/Folder'; 
 
 
+  function registerRquest(method,url,response,authorization){
+      $httpBackend.when(method,url)
+        .respond(function(method, url, data, headers){
+          expect(authorization).to.deep.equal(headers.Authorization);
+          return [200 , response];
+        });
+  }
+
   beforeEach(module('dropboxModule'));
   beforeEach(inject(function(_dropboxService_ , _$httpBackend_){
 
     dropboxService = _dropboxService_;
     $httpBackend = _$httpBackend_;
-
-    $httpBackend.whenPOST(urlForToken)
-      .respond(function(method, url, data, headers){
-        return [200 , responseAccessToken];
-      });
-
-    $httpBackend.whenGET(urlForProfileInfo)
-      .respond(function(method, url, data, headers){
-        expect(authorization).to.deep.equal(headers.Authorization);
-        return [200 , responseProfileInfo];
-      });
-
-    $httpBackend.whenGET(uarForFolderInfo)
-      .respond(function(method, url, data, headers){
-        expect(authorization).to.deep.equal(headers.Authorization);
-        return [200 , responseFolderInfo];
-      });
-      //вынести в одну функцию.
   }));
 
-  it('should be able to return the correct link authorization', function(){
-      var appKey = [ Math.random() , Math.random() , Math.random() ].join('');
-      var correctLink = "https://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=" + appKey;
-      expect(dropboxService.getLinkForToken(appKey)).to.deep.equal(correctLink);
-  });
 
-  it('should be able to correctly receive the token', function(){
-      dropboxService.getPromiseToken(secret,appKey,appSecret).then(function(info){
-        expect(info.data).to.deep.equal(responseAccessToken);
-      });
-      $httpBackend.flush();
-  });
+  describe('dropboxService ',function(){
 
-  it('should be able to correctly receive the profile info', function(){
-      dropboxService.getProfileInfo(access_token).then(function(info){
-        expect(info.data).to.deep.equal(responseProfileInfo);
-      });
-      $httpBackend.flush();
-  });
+    it('should be able to return the correct link authorization', function(){
+        var appKey = [ Math.random() , Math.random() , Math.random() ].join('');
+        var correctLink = "https://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=" + appKey;
+        expect(dropboxService.getLinkForToken(appKey)).to.deep.equal(correctLink);
+    });
 
-  it('should be able to correctly receive the folder info', function(){
+    it('should be able to correctly receive the token', function(){
+        registerRquest('POST',urlForToken,responseAccessToken);
 
-  });
+        dropboxService.getPromiseToken(secret,appKey,appSecret).then(function(info){
+          expect(info.data).to.deep.equal(responseAccessToken);
+        });
+        $httpBackend.flush();
+    });
+
+    it('should be able to correctly receive the profile info', function(){
+        registerRquest('GET',urlForProfileInfo,responseProfileInfo,authorization);
+
+        dropboxService.getProfileInfo(access_token).then(function(info){
+          expect(info.data).to.deep.equal(responseProfileInfo);
+        });
+        $httpBackend.flush();
+    });
+
+    it('should be able to correctly receive the folder info', function(){
+        registerRquest('GET',uarForFolderInfo,responseFolderInfo,authorization);
+
+        dropboxService.getFolderInfo('/Folder',access_token).then(function(info){
+          expect(info.data).to.deep.equal(responseFolderInfo);
+        });
+        $httpBackend.flush();
+    });
+
+  })
 
   afterEach(function(){
     
