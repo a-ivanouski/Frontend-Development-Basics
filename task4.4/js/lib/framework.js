e795c7c3af7275f84f9f351b34e09b8c4c47752f
Dzmitry Patseyuk <Dzmitry.Patseyuk@aerlingus.com>
2015-07-29 10:21:33 +0300

    done: click and model

--- task4.4/js/lib/framework.js
+++ task4.4/js/lib/framework.js
@@ -1,40 +1,79 @@
-var model = [
-	{
-		prop1:"1111111"
-	},
-	{
-		prop2:"22222222"
-	}
-
-];
-
-var behavior = [];
+var behaviors = {};
 
 function createSelfBehavior(){
+
 	return {
-		name:"",
-		_self:{}
+		name: "",
+		self: {},
+		element: null,
 	}
 }
 
+function getAttributeValue(element,attributeName){
 
-function createBehavior(name,models,constructor){
-	if(!behavior[name] && constructor instanceof Function){
+	return element.attributes[attributeName] ? element.attributes[attributeName].nodeValue : null;
+}
 
-		behavior[name] = createSelfBehavior();
-		var params = [behavior[name]._self].concat(models);
+function updateClick(behavior,element,attribute,parent){
+
+	attribute = attribute.replace( /\s/g, "").split(/['(',')']/);
+
+	if(behavior.self[attribute[0]]){
+		var funct = behavior.self[attribute[0]];
+		var paramsNames = attribute.splice(1);
+
+		var params = [];
+		for(var i=0;i<paramsNames.length;i++){
+			if(paramsNames[i] !== "")
+				params[i] = behavior.self[paramsNames[i]];
+		}
+
+		element.onclick = function(){
+			funct.apply(null,params);
+			updateFrameworkModel(behavior,parent);
+		};
+	}
+}
+
+function updateFrameworkModel(behavior,element){
+	var innerElements = element.querySelectorAll('*');
+	for(var i =0; i< innerElements.length; i++){
+
+		var attrModel = getAttributeValue(innerElements[i],'framework-model');
+		var attrClick = getAttributeValue(innerElements[i],'framework-click');
+		if(behavior.self[attrModel]){
+			innerElements[i].innerHTML = behavior.self[attrModel];
+		}
+		if(attrClick){
+			updateClick(behavior,innerElements[i],attrClick,element)
+		}
+	}
+}
+
+function render(behavior,element){
+
+	console.log("render " + behavior.name);
+
+	updateFrameworkModel(behavior,element);
+}
+
+function createBehavior(name,constructor){
+
+	if(!behaviors[name]){
+		behaviors[name] = createSelfBehavior();
+		behaviors[name].name = name;
+		var params = [behaviors[name].self];
 		constructor.apply(null,params);
 	}
-	else {
-		throw new Error("behavior already exist!!!");
-	}
 }
 
+window.onload = function(e){
+	var elements = document.querySelectorAll('[behavior]');
+	for(var i=0; i<elements.length; i++){
 
-createBehavior("FirstBehavior",[model[0],model[1]], function(self,model1,model2){
-	self.value1 = model1.prop1;
-	self.value2 = model2.prop2;
-})
-
-console.log(behavior["FirstBehavior"]._self);
-
+		var nameBehavior =  getAttributeValue(elements[i],'behavior');
+		if(behaviors[nameBehavior]){
+			render(behaviors[nameBehavior],elements[i])
+		}
+	}
+}
---
+++ task4.4/js/script/script.js
@@ -0,0 +1,13 @@
+createBehavior("FirstBehavior", function(self){
+
+	self.value1 = 'click me!';
+	self.value2 = 1;
+
+	self.func = function (v2,v3){
+		v2++;
+		v3++;
+		self.value2 = v2;
+		self.value3 = self.value2 + 1;
+	}
+
+})
