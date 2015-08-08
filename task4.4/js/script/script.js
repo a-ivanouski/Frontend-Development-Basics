4ce2c1d261d34a4a15baef373d67e427615a0074
dimaPateuk <dima_pateuk@mail.ru>
2015-08-08 08:32:47 +0300

    described framework model

--- task4.4/js/script/script.js
+++ task4.4/js/script/script.js
@@ -1,11 +1,14 @@
 createBehavior("frameworkModelBehavior", function(self){
-	
-
-
+	self.value = "This is my framework value!!!";
 })
 
 createBehavior("frameworkModelObjectBehavior", function(self){
 
+	self.obj = {
+		value: {
+			value: "This is obj.value.value"
+		}
+	};
 
 
 })
