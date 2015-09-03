21e99f3aa2a6929d21958757ff505938ca18cb58
Dzmitry Patseyuk <Dzmitry.Patseyuk@aerlingus.com>
2015-09-03 18:02:15 +0300

    added ui router

--- Angular/js/script.js
+++ Angular/js/script.js
@@ -11,4 +11,33 @@ angular.module('store',[
 		'selected',
 		'elements',
 		'available-item',
+		'ui.router'
 	])
+
+
+angular.module('store')
+	.controller('test',['$scope','$state','$stateParams',function($scope,$state,$stateParams){
+		console.log(123);
+		$state.go('home/items',{value:1});
+	}])
+	.config(function($stateProvider,$urlRouterProvider){
+		$stateProvider
+			.state('home/items',{
+				url:'home/items',
+				resolve:{
+					initialData: function(){
+						console.log('home/items');
+					}
+				}
+			})
+			.state('home/items/selected',{
+				url:'home/items/selected',
+				resolve:{
+					initialData: function(){
+						console.log('home/items/selected');
+
+					}
+				}
+			})
+		$urlRouterProvider.otherwise("/home/items");
+	})
