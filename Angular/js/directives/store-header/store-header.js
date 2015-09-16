8577373f64eb71a0a503aa01af25064df5f9680e
Dzmitry Patseyuk <Dzmitry.Patseyuk@aerlingus.com>
2015-09-16 17:12:26 +0300

    made authorize

--- Angular/js/directives/store-header/store-header.js
+++ Angular/js/directives/store-header/store-header.js
@@ -5,11 +5,23 @@ angular.module('header')
 			templateUrl:'js/directives/store-header/store-header.html',
 			link: function(scope,element,attr){
 				scope.click = login.LOGIN;
+
+				scope.$watch('$root.user', function (newVal, oldVal) {
+					console.log(newVal);
+					if(newVal){
+						scope.fullName = newVal.fullName;
+					}
+				})
 			}
 		}
 	}])
-	.service('login', ['$state', function($state) {
+	.service('login', ['$state', '$injector', function($state, $injector) {
+			var rootScope = $injector.get('$rootScope'),
+			$sessionStorage = $injector.get('$sessionStorage');
 			this.LOGIN = function() {
+				$sessionStorage.user = {
+					fullName: 'Patseyuk Dima'
+				};
 				console.log("LOGIN!!!");
 			}
 	}])
--- Angular/js/script.js
+++ Angular/js/script.js
@@ -10,29 +10,36 @@ angular.module('store', [
 		'selected',
 		'elements',
 		'available-item',
-		'ui.router'
+		'ui.router',
+		'ngStorage'
 	])
 
 angular.module('store')
 	.controller('test', ['$scope', '$state', 'login', function ($scope, $state, login) {
-		// login.LOGIN();
-		// 		$state.go('home')
-
 
 	}])
 	.config(function($stateProvider, $urlRouterProvider){
 		$stateProvider
 			.state('home', {
 				url: '/home',
-				templateUrl: 'views/home.html'
+				templateUrl: 'views/home.html',
+				data: {
+					'noLogin': true
+				},
 			})
 			.state('cars', {
 				url: '/cars',
-				templateUrl: 'views/cars.html'
+				templateUrl: 'views/cars.html',
+				data: {
+					'noLogin': true
+				},
 			})
 			.state('cars.id', {
 				url: 'cars/:id',
 				templateUrl: 'views/selectedCar.html',
+				data: {
+					'noLogin': true
+				},
 				resolve: {
 					initialData: function() {
 					}
@@ -40,9 +47,46 @@ angular.module('store')
 			})
 			.state('profile', {
 				url: '/profile',
-				templateUrl: 'views/profile.html'
+				templateUrl: 'views/profile.html',
+
 			})
 
 		$urlRouterProvider.otherwise('/home');
 	})
+	.run(['$rootScope', '$state', '$stateParams', 'SessionService',	function ($rootScope, $state, $stateParams, SessionService) {
+			$rootScope.$state = $state;
+			$rootScope.$stateParams = $stateParams;
+
+			$rootScope.user = null;
+
+			// Здесь мы будем проверять авторизацию
+			$rootScope.$on('$stateChangeStart',	function (event, toState, toParams, fromState, fromParams) {
+				SessionService.checkAccess(event, toState, toParams, fromState, fromParams);
+			});
+		}
+	])
+	.service('SessionService', ['$injector', function($injector) {
+
+		this.checkAccess = function(event, toState, toParams, fromState, fromParams) {
+			var $scope = $injector.get('$rootScope'),
+			$sessionStorage = $injector.get('$sessionStorage');
+
+			if (toState.data !== undefined) {
+				if (toState.data.noLogin !== undefined && toState.data.noLogin) {
+
+				}
+			} else {
+				// вход с авторизацией
+				if ($sessionStorage.user) {
+					$scope.$root.user = $sessionStorage.user;
+				} else {
+				// если пользователь не авторизован - отправляем на страницу авторизации
+				event.preventDefault();
+				alert('Please LOGIN');
+					$scope.$state.go('home');
+				}
+			}
+		};
+		}
+	]);
 
