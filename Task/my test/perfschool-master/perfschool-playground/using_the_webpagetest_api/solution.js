dba28dc401f62cbdea9fe69bd154ddd14b6c68f1
Dzmitry Patseyuk <dzmitry_patseyuk@epam.com>
2015-12-09 14:56:55 +0300

    second task complete

--- Task/my test/perfschool-master/perfschool-playground/using_the_webpagetest_api/solution.js
+++ Task/my test/perfschool-master/perfschool-playground/using_the_webpagetest_api/solution.js
@@ -5,14 +5,15 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 7777;
+var localtunnel = require('localtunnel');
+var WebPageTest = require('webpagetest');
+var wpt = new WebPageTest('www.webpagetest.org', 'A.5b0265588a1abe6b0a7e96e444c34546');
+
+var handle;
 
 app.get('/', home);
 app.get('/test', test);
 app.listen(port, listening);
-
-function listening () {
-  console.log('Listening on port', port);
-}
 
 function home (req, res) {
   var file = path.join(__dirname, 'index.html');
@@ -21,5 +22,31 @@ function home (req, res) {
 }
 
 function test (req, res) {
-  res.end('Not very testful!');
+  var tunnel = localtunnel(port, function(err, tunnel) {
+
+    if(err) return;
+    var testId;
+    wpt.runTest(tunnel.url, {location: 'ec2-us-west-2:Firefox'}, function(err, data) {
+        testId = data.data.testId;
+        handle = setInterval(requestResults, 6000);
+    });
+
+    function requestResults() {
+        wpt.getTestResults(testId, function (err, data) {
+            var testResults = data;
+            console.log(data.statusText);
+            if(data.statusCode === 200) {
+                var testResults = data.data.average.firstView;
+                res.json({
+                    'timing': {
+                        'ttfb': testResults.TTFB,
+                        'speedIndex': testResults.SpeedIndex,
+                        'domLoaded': testResults.domContentLoadedEventStart
+                    }
+                });
+                clearInterval(handle);
+            }
+        }); 
+    }
+  });
 }
